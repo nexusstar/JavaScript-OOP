98d594e34c7e6acc569941b88b18d9e92390cfb0
Petar Nikov <petar.nikov@gmail.com>
2015-06-24 18:26:13 +0300

    work in progress on Classical Inheritance

--- 04. Classical Inheritance/homework/tasks/task-1.js
+++ 04. Classical Inheritance/homework/tasks/task-1.js
@@ -17,11 +17,71 @@
 */
 function solve() {
 	var Person = (function () {
-		function Person() {
+		
+		function isString(obj){
+			return (typeof obj === 'string');
 		}
 		
+		function isName(name){
+			return /^[a-zA-Z]{3,20}$/.test(name);
+		}
+		
+		function Person(firstname, lastname, age) {
+				this.firstname = firstname;
+				this.lastname = lastname;
+				this.age = age;	
+		}
+		Person.protoype.firstname = {
+			get firstname(){
+				return this.firstname;
+			},
+			set firstname(name){
+				if(!name || !isString(name) || isName(name)){
+					throw "Error: ilegal first name provided";
+				}
+				this.firstname = name;
+			}
+		}
+		
+		Person.prototype.lastname = {
+			
+			get lastname(){
+				return this._lastname;
+			},
+			set lastnane(name){
+				if(!name || !isString(name) || isName(name)){
+					throw "Error: ilegal last name provided";
+				}
+				this.lastname = name; 
+			}
+		}
+		
+		Person.prototype.age = {
+			get age(){
+				return this._age;
+			},
+			set age(num){
+				if(!age || isNaN(age) || age < 0 || age > 150){
+					throw "Error ilegal age provided"
+				}
+			}
+		}
+		
+		Person.prototype.fullname ={
+				get: this._firstname + ' ' + this._lastname,
+				set: function(name){
+					var names = name.split(' ');
+					this._firstname = names[0];
+					this._lastname = names[1];			
+			}
+		};
+			
 		return Person;
 	} ());
+	var p = new Person("firstname", "lastname", 120);
+	console.log(p);
+	console.log(p.fullname());
 	return Person;
 }
-module.exports = solve;
+solve();
+// module.exports = solve;
